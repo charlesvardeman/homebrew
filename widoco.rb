887ceb2cac5d08a6ef95e743754d56e8191cdd25
Charles Vardeman <charles.vardeman@gmail.com>
2020-09-08 08:47:37 -0400

    Updating widoco to latest version

--- widoco.rb
+++ widoco.rb
@@ -2,9 +2,9 @@
 class Widoco < Formula
   desc "Wizard for documenting ontologies. WIDOCO is a step by step generator of HTML templates with the documentation of your ontology. It uses the LODE environment to create part of the template."
   homepage "https://zenodo.org/badge/latestdoi/11427075"
-  version "1.4.9"
-  url "https://github.com/dgarijo/Widoco/releases/download/v1.4.9/widoco-1.4.9-jar-with-dependencies.jar"
-  sha256 "854242bc031f2c6703258d309cfcf644b99517112ace5cfd746197a696898728"
+  version "1.4.14"
+  url "https://github.com/dgarijo/Widoco/releases/download/v1.4.9/widoco-1.4.14-jar-with-dependencies.jar"
+  sha256 "ce0422906e36fdfa4fa54ef75fccef6c087c00d155dc2366977b259b938328c8"
 
   bottle :unneeded
   depends_on :java => "1.8+"
@@ -27,6 +27,6 @@ class Widoco < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    assert_match version.to_s, shell_output("#{bin}/widoco --version")
+   system "#{bin}/widoco", "--version"
   end
 end
